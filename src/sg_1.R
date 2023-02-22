e32499ccf0135d4bc2981e31ef3c62c63a03d6ac
Gauthier (GEN) <nicolas.gauthier@beneva.ca>
2023-02-22 13:19:33 -0500

    new sg_1

--- src/sg_1.R
+++ src/sg_1.R
@@ -1,1 +1,5 @@
+set(input_dt, j = "age_princ_drv", value = calc_age(input_dt[["DATE_FROM"]], input_dt[["DATE_TO"]]))
+
 set(input_dt, j = "nbr_claim_tar_princ_drv_tot_1yr", value = sum_claims(input_dt[["claim_tar_princ_drv_tot_1yr_ind"]]))
+
+set(input_dt, j = "id", value = paste0(input_dt[["VEH_NO_POLICE"]], input_dt[["VEH_NO"]]))
