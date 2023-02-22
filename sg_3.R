17672918d2956a16f5de230d4b94c43b69caa67f
Gauthier (GEN) <nicolas.gauthier@beneva.ca>
2023-02-22 10:31:46 -0500

    sg_3

---
+++ sg_3.R
@@ -0,0 +1,4 @@
+experience4model <- setDT(read_object("Experience4model4terr.rds"))
+
+### Sélectionner les observation où il y a des sinistres ###
+dt_4model <- experience4model[UNI_AUTRE != 0 & YEAR < 2020 & MNT_ECR_AUTRE > 0,]
