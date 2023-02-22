058752291dae17b1de0c11e360b09abdf2e249c1
Francois Turcotte <francois.turcotte@lacapitale.com>
2023-02-22 11:18:33 -0500

    Corrigé l'erreur de styleguide #1

--- sg_1.R
+++ sg_1.R
@@ -1,1 +1,4 @@
 set(input_dt, j = "nbr_claim_tar_princ_drv_tot_1yr", value = nbr_claim_calculation(input_dt[["claim_tar_princ_drv_tot_1yr_ind"]]))
+
+set(input_dt, j = "nbr_claim_tar_princ_drv_tot_1yr", value = calc_nbr_claim(input_dt[["claim_tar_princ_drv_tot_1yr_ind"]]))
+
