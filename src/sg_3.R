8e5676b1a21b4cd25bf4747ae7e8da0204d5178d
Gauthier (GEN) <nicolas.gauthier@beneva.ca>
2023-02-22 13:29:58 -0500

    new sg_3

--- src/sg_3.R
+++ src/sg_3.R
@@ -1,7 +1,26 @@
 experience_dt <- setDT(read_object("Experience4model4terr.rds"))
 
 ### Sélectionner les observation où il y a des sinistres ###
-model_dt <- experience_dt[UNI_AUTRE != 0 & YEAR < 2020 & MNT_ECR_AUTRE > 0,]
+model_dt <- experience_dt[UNI_AUTRE != 0 & YEAR < 2020 & MNT_ECR_AUTRE > 0]
 
 #En temps normal, jaurais dis que year aurait du etre en minuscule, mais dans ce cas-ci, le data venait de SAS et year est en majuscule, j’accepte les deux
+
+
+#Quelles variables proviennent de l'entrepot
+
+prod_age
+INTE_EXPCONVL
+nbr_claim_tot_6yrs
+PROD_LONGITUDE
+FAM_NBR_NV
+driving_exp_princ_drv
+VALEUR
+ROLE_SECTEMPL_occ_drv
+
+# réponse
+
+INTE_EXPCONVL
+PROD_LONGITUDE
+FAM_NBR_NV
+VALEUR
 
