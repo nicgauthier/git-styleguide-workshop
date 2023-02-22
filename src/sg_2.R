93a74db07a29cbf51da249a8ccdc803b5284e9fb
Gauthier (GEN) <nicolas.gauthier@beneva.ca>
2023-02-22 13:59:59 -0500

    fix corrigé for 2 and 4

--- src/sg_2.R
+++ src/sg_2.R
@@ -1,10 +1,10 @@
 dt_list <- tests.data::inforce_ppv_after_pp
 
 
-nbr_lines <- dt_list[["products"]][ , .N]
+nbr_lines <- dt_list[["products"]][, .N]
 dt_list[["products"]][,  row_nbr := 1:.N]
 
-dt_list[["products"]][ , ind_prod_age_eq_0 := as.numeric(prod_age == 0)]
+dt_list[["products"]][, ind_prod_age_eq_0 := as.numeric(prod_age == 0)]
 
-dt_list[["products"]][ , ind_prod_annual_dist_km_gt_40 := as.numeric(prod_annual_dist_km > 40)]
+dt_list[["products"]][, ind_prod_annual_dist_km_gt_40 := as.numeric(prod_annual_dist_km > 40)]
 
