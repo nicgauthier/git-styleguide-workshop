31f14ba9c21ef4a53b094bedbe466af70217c988
Gauthier (GEN) <nicolas.gauthier@beneva.ca>
2023-02-22 10:47:11 -0500

    sg6 corrigé

--- src/sg_6.R
+++ src/sg_6.R
@@ -1,3 +1,11 @@
+# Problèmes :
+#   -	Nom de la variable qui ne dit rien et en français.
+# -	Majuscule au nom de la variable.
+# -	Ligne trop longue et laide, indenter.
+# 
+# Devrait être de quoi comme :
+#   
+
 data.table::set(products_dt, 
                 j = "categ_prod_age", 
                 value = data.table::fifelse(products_dt$prod_age <= 5, 1, 
