093aed1bcb070135d8fb5966a033467d4babe835
Gauthier (GEN) <nicolas.gauthier@beneva.ca>
2023-02-22 13:41:03 -0500

    new sg_5

--- sg_5.R
+++ sg_5.R
@@ -1,2 +1,14 @@
-nombre_ligne <- list_dt$products[,  .N]
-list_dt$products[,  nombre_ligne := .N]
+from = as.Date("2022-01-01")
+to = as.Date("2023-01-01")
+
+data <- get_claims(from=from,to=to,cie="beneva")
+
+tot <- data[, sum(ENCOURU_TOTAL) ]
+
+is_over_budget <- F
+
+if(tot > 500000) {
+  is_over_budget <- T}
+
+
+
