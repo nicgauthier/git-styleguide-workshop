adaf6da13a2dcd61928cb5373140dbc533d64db3
Gauthier (GEN) <nicolas.gauthier@beneva.ca>
2023-02-22 10:34:49 -0500

    no return for age

--- src/sg_4.R
+++ src/sg_4.R
@@ -13,5 +13,6 @@
     age <- to_lt$year - from_lt$year
     lines_to_change <- which(to_lt$mon < from_lt$mon | (to_lt$mon == from_lt$mon&to_lt$mday < from_lt$mday))
     age[lines_to_change] <- age[lines_to_change] - 1
-    return(age)
+    
+    age
   }
