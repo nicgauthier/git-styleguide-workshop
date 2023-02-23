ad0b0cff3be00a2d7f0c624c77474145edc73318
sdeschatelets <simon.deschatelets@lacapitale.com>
2023-02-23 15:47:15 -0500

    Correction erreur 2

--- sg_2.R
+++ sg_2.R
@@ -1,11 +1,11 @@
 list_dt <- tests.data::inforce_ppv_after_pp
 
 
-nombre_ligne <- list_dt$products[,  .N]
-list_dt$products[,  nombre_ligne := .N]
+nbr_lines <- list_dt[["products"]][,  .N]
+list_dt[["products"]][,  nbr_lines := .N]
 
-list_dt$products$indicateur_age_a_0 <- 0
-list_dt$products[prod_age == 0, indicateur_age_a_0 := 1]
 
-list_dt$products$prod_annual_dist_km_plus_grand_40 <- 0
-list_dt$products[prod_annual_dist_km > 40, prod_annual_dist_km_plus_grand_40 := 1]
+list_dt[["products"]][, ind_prod_age_eq_0 := as.numeric(prod_age == 0)]
+
+list_dt[["products"]][["prod_annual_dist_km_gt_40"]] <- 0
+list_dt[["products"]][prod_annual_dist_km > 40, prod_annual_dist_km_gt_40 := 1]
