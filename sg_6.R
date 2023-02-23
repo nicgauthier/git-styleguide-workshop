8b2ea088e6e544a67c59f8b0f9aebd77f29ce23e
Gauthier (GEN) <nicolas.gauthier@beneva.ca>
2023-02-23 08:32:57 -0500

    new sg_6

--- sg_6.R
+++ sg_6.R
@@ -1,1 +1,19 @@
-•	data.table ::set(products_dt, j =  ‘’Bidon’’, value = data.table ::fifelse(products_dt$prod_age <= 5, 1, data.table ::fifelse(products_dt$prod_age > 5 & products_dt$prod_age <= 10, 2, data.table ::fifelse(products_dt$prod_age > 10 & products_dt$prod_age <= 15, 3, 4))))
+ExpDt[, Date_Achat_Btm := as.Date(paste(SIT_DATE_OCCUPATION, "/01", sep=""), format = "%Y/%m/%d")]
+
+
+if (nrow(sample_dt) > 0) {
+  warning(paste0("The following policies will be removed : ", paste0(sample_dt[["SIT_NO_POLICE"]], collapse = ", "), "."))
+  input_dt_list <- ugen.utils::filter_situations(input_dt_list, !(SIT_NO_POLICE %chin% sample_dt[["SIT_NO_POLICE"]]))
+}
+
+
+age_drivers <- function(input_dt, ref_date_var){
+  set(input_dt, j = "age", value = calc_age(input_dt[["IND_DATE_NAISSANCE"]], input_dt[[ref_date_var]]))
+}
+
+
+prep_train <- freq_entree_eau_prep_fun(train,outcome_var=outcome_var,predictors_vars=predictors_vars,offset_var=offset_var,use_train=TRUE)
+
+
+
+flex_pct<-round(profit_flex*100,2)+1
