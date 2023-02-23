f08442db4942f24c2cbfd27e8fc57dd958f85878
Gauthier (GEN) <nicolas.gauthier@beneva.ca>
2023-02-23 08:30:28 -0500

    new sg_6

--- src/sg_6.R
+++ src/sg_6.R
@@ -1,13 +1,27 @@
-# Problèmes :
-#   -	Nom de la variable qui ne dit rien et en français.
-# -	Majuscule au nom de la variable.
-# -	Ligne trop longue et laide, indenter.
-# 
-# Devrait être de quoi comme :
-#   
+## Variable calculées : On utilise le contexte et un nom significatif en minuscule
 
-data.table::set(products_dt, 
-                j = "categ_prod_age", 
-                value = data.table::fifelse(products_dt$prod_age <= 5, 1, 
-                                            data.table::fifelse(products_dt$prod_age > 5 & products_dt$prod_age <= 10, 2, 
-                                                                data.table::fifelse(products_dt$prod_age > 10 & products_dt$prod_age <= 15, 3, 4))))
+exp_dt[, risk_occupation_date := as.Date(paste(SIT_DATE_OCCUPATION, "/01", sep=""), format = "%Y/%m/%d")]
+
+
+## Indentation
+
+if (nrow(sample_dt) > 0) {
+  warning(paste0("The following policies will be removed : ", paste0(sample_dt[["SIT_NO_POLICE"]], collapse = ", "), ".")) 
+  input_dt_list <- ugen.utils::filter_situations(input_dt_list, !(SIT_NO_POLICE %chin% sample_dt[["SIT_NO_POLICE"]]))
+}
+
+
+## On doit utiliser des verbes d'actions
+
+calc_drivers_age <- function(input_dt, ref_date_var) {
+  set(input_dt, j = "age", value = calc_age(input_dt[["IND_DATE_NAISSANCE"]], input_dt[[ref_date_var]]))
+}
+
+
+## Always put a space after a comma, never before, just like in regular English
+prep_train <- freq_entree_eau_prep_fun(train, outcome_var = outcome_var, predictors_vars = predictors_vars, offset_var = offset_var, use_train = TRUE)
+
+## Put some spaces
+
+flex_pct <- round(profit_flex * 100, 2) + 1
+
