7ec697aa7af125fbf6df6c39969a91434d1ca150
Samuel Sauvageau <samuel.sauvageau@lacapitale.comm>
2023-02-23 15:46:48 -0500

    Correction du styleguide

--- sg_3.R
+++ sg_3.R
@@ -1,17 +1,17 @@
-experience4model <- setDT(read_object("Experience4model4terr.rds"))
+exp_dt <- setDT(read_object("Experience4model4terr.rds"))
 
 ### Sélectionner les observation où il y a des sinistres ###
-dt_4model <- experience4model[UNI_AUTRE != 0 & YEAR < 2020 & MNT_ECR_AUTRE > 0,]
+exp_dt <- exp_dt[UNI_AUTRE != 0 & YEAR < 2020 & MNT_ECR_AUTRE > 0,]
 
 
 
 #Quelles variables proviennent de l'entrepot
 
 prod_age
-INTE_EXPCONVL
+INTE_EXPCONVL #entrepot
 nbr_claim_tot_6yrs
-PROD_LONGITUDE
-FAM_NBR_NV
+PROD_LONGITUDE #entrepot
+FAM_NBR_NV #entrepot
 driving_exp_princ_drv
-VALEUR
-ROLE_SECTEMPL_occ_drv
+VALEUR #entrepot
+ROLE_SECTEMPL_occ_drv #entrepot
