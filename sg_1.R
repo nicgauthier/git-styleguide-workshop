39210642814b5c78ac8373a384382a644d4c5eea
Gauthier (GEN) <nicolas.gauthier@beneva.ca>
2023-02-23 09:13:15 -0500

    add parenthesis

--- sg_1.R
+++ sg_1.R
@@ -2,4 +2,4 @@ set(input_dt, j = "AGE_PRINC_DRV", value = calc_age(input_dt[["DATE_FROM"]], inp
 
 set(input_dt, j = "nbr_claim_tar_princ_drv_tot_1yr", value = nbr_claim_calculation(input_dt[["claim_tar_princ_drv_tot_1yr_ind"]]))
 
-set(gros.data, j = "id", value = paste0(gros.data[["VEH_NO_POLICE"]], gros.data[["VEH_NO"]])
+set(gros.data, j = "id", value = paste0(gros.data[["VEH_NO_POLICE"]], gros.data[["VEH_NO"]]))
