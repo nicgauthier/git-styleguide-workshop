08f4cb5a53f52f4d687f34e1abdb6aa5f994dcaa
Jean Forest <jean.forest@lacapitale.com>
2023-02-23 16:11:36 -0500

    Changer $ pour [[

--- sg_2.R
+++ sg_2.R
@@ -1,11 +1,11 @@
 list_dt <- tests.data::inforce_ppv_after_pp
 
 
-nombre_ligne <- list_dt$products[,  .N]
-list_dt$products[,  nombre_ligne := .N]
+nombre_lignes <- list_dt$products[,  .N]
+list_dt[["products"]][, line_nbr := 1:nombre_lignes]
 
-list_dt$products$indicateur_age_a_0 <- 0
-list_dt$products[prod_age == 0, indicateur_age_a_0 := 1]
 
-list_dt$products$prod_annual_dist_km_plus_grand_40 <- 0
-list_dt$products[prod_annual_dist_km > 40, prod_annual_dist_km_plus_grand_40 := 1]
+list_dt[["products"]][, age_eq_0_ind := as.numeric(prod_age == 0)]
+
+
+list_dt[["products"]][, prod_annual_dist_km_gt_40 := as.numeric(prod_annual_dist_km > 40)]
