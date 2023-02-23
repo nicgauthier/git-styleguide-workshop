538995a4488db96f8cf894325634c4183ee55bc2
ViLy47778 <phuong-vi.ly@lacapitale.com>
2023-02-23 16:06:36 -0500

    Corriger erreur

--- sg_3.R
+++ sg_3.R
@@ -1,17 +1,16 @@
-experience4model <- setDT(read_object("Experience4model4terr.rds"))
+exp_dt <- setDT(read_object("Experience4model4terr.rds"))
 
 ### Sélectionner les observation où il y a des sinistres ###
-dt_4model <- experience4model[UNI_AUTRE != 0 & YEAR < 2020 & MNT_ECR_AUTRE > 0,]
+sin_dt <- exp_dt[UNI_AUTRE != 0 & YEAR < 2020 & MNT_ECR_AUTRE > 0,]
 
 
 
 #Quelles variables proviennent de l'entrepot
 
-prod_age
+
 INTE_EXPCONVL
-nbr_claim_tot_6yrs
+
 PROD_LONGITUDE
 FAM_NBR_NV
-driving_exp_princ_drv
+
 VALEUR
-ROLE_SECTEMPL_occ_drv
